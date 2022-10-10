5fab1ced75ef027507b0ea6228d16e386f0b0af9
Trent Manager <tmanager98@gmail.com>
2022-10-10 10:43:54 -0700

    Adding more info to experiance

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -22,6 +22,10 @@ const Experience = () => {
                     <ul>
                         <li>Successfully migrated pull-request for stage two code review & API connections for Web3 users</li>
                         <li>Peer reviewed &amp; Audited Solidity code</li>
+
+                        <i>Patreon Technology</i> - <strong>Position/Title:  Solidity Engineer & Python Developer (Contractor)</strong>, Dates:  3/15/2020 - 6/1/2021
+                        <i>Performed weekly blockchain training sessions for Patreon Community.</i>
+                        <li>Assessed cyber secuirty & primitive technical planning &amp; Deployed Solidity Smart Contracts for Patreon Community</li>
                     </ul>
                 </li>
             </ul>
